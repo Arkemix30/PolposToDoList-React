e9310451a85a7920e1b6f7a15db7e7f117df92f1
Enmanuel Silva <silvacompte@gmail.com>
2021-02-03 10:37:41 -0600

    Commenting every single function for better understanding

--- src/components/FormTodo/FormTodo.js
+++ src/components/FormTodo/FormTodo.js
@@ -4,18 +4,23 @@ import './FormTodo.css'
 const FormTodo = (props) => {
 
         const [inputText, setInputText] = useState('');
-        
+
+        //OnChangeHandler to get the current value of the form field.
         const OnChangeHandler = (e) => {
             setInputText(e.target.value);
         };
-
+        //A submit handler to pass the data from this component to ToDoList component.
         const SubmitHandler = (e) => {
+            //method that cancels the page reload by submitting the event
             e.preventDefault();
             props.onSubmit({
+                //Random Custom ID
                 id:Math.random() * 555784 + 1,
+                //Getting the Value of the field
                 text: inputText,
                 completed: false
             })
+            //Setting the input text field to blank field for new use
             setInputText('');
         };
 
--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ToDo.css'
 const Todo = ({ todos, completeTodo, removeTodo }) => {
+    //Rendering every single TODO from the TODOS array
     return todos.map((todo, index,) => (
         <div className="todo container" key={index}>
             <div className="row justify-content-center">
                 <div className="col col-md-10">
+                    {/* Custom Validation to change CSS properties of the list */}
                     <li className={todo.completed ? 'list-group-item completed' : 'list-group-item'} key={todo.id} onClick={() => completeTodo(todo.id)}>{todo.text}</li>
                 </div>
                 <div className="col-xs-1 align-self-center">
